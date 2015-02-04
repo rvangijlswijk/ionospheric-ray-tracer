98492ab570352847520c55d5c8073fb800ca335b
Rian van Gijlswijk <rian@vangijlswijk.com>
2015-02-04 16:28:38 +0200

    Fixed bug in intersection calculation

--- src/math/Line2f.cpp
+++ src/math/Line2f.cpp
@@ -5,6 +5,7 @@
  *      Author: rian
  */
 
+#include <iostream>
 #include <cmath>
 #include "Line2f.h"
 #include "Vector2f.h"
@@ -39,14 +40,39 @@ namespace math {
 	Vector2f Line2f::intersect(Line2f line) {
 
 		Vector2f intersection;
-		intersection.x = ((begin.x * end.y - begin.y * end.x) * (line.begin.x - line.end.x)
-			- (begin.x - end.x) * (line.begin.x * line.end.y - line.begin.y * line.end.x))
-			/((begin.x - end.x) * (line.begin.y - line.end.y) - (begin.y - end.y) * (line.begin.x - line.end.x));
 
-		intersection.y = ((begin.x * end.y - begin.y * end.x) * (line.begin.y - line.end.y)
-				- (begin.y - end.y) * (line.begin.x * line.end.y - line.begin.y * line.end.x))
-				/((begin.x - end.x) * (line.begin.y - line.end.y) - (begin.y - end.y) * (line.begin.x - line.end.x));
+		// Store the values for fast access and easy
+		// equations-to-code conversion
+		double x1 = begin.x, x2 = end.x, x3 = line.begin.x, x4 = line.end.x;
+		double y1 = begin.y, y2 = end.y, y3 = line.begin.y, y4 = line.end.y;
 
+		// calculate determinant
+		double d = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+
+		// If the determinant is zero, there is no intersection
+		if (d == 0) {
+			return intersection;
+		}
+
+		// Get the x and y
+		double pre = (x1*y2 - y1*x2), post = (x3*y4 - y3*x4);
+		double x = ( pre * (x3 - x4) - (x1 - x2) * post ) / d;
+		double y = ( pre * (y3 - y4) - (y1 - y2) * post ) / d;
+
+		// Check if the x and y coordinates are within both lines
+		double epsilon = 1e-5;
+		if (x < (min(x1, x2) - epsilon) || x > (max(x1, x2) + epsilon) ||
+			x < (min(x3, x4) - epsilon) || x > (max(x3, x4) + epsilon)) {
+		        return intersection;
+		}
+		if (y < (min(y1, y2) - epsilon) || y > (max(y1, y2) + epsilon) ||
+			y < (min(y3, y4) - epsilon) || y > (max(y3, y4) + epsilon)) {
+		        return intersection;
+		}
+
+		// Return the point of intersection
+		intersection.x = x;
+		intersection.y = y;
 
 		return intersection;
 	}
@@ -54,11 +80,11 @@ namespace math {
 	/**
 	 *
 	 */
-	float Line2f::angularDifference(Line2f line) {
+	double Line2f::angularDifference(Line2f line) {
 
-		float thisSlope = (end.y - begin.y)/(end.x - begin.x);
-		float otherSlope = (line.end.y - line.begin.y)/(line.end.x - line.begin.x);
-		float a = abs((thisSlope - otherSlope)/(1 + thisSlope * otherSlope));
+		double thisSlope = (end.y - begin.y)/(end.x - begin.x);
+		double otherSlope = (line.end.y - line.begin.y)/(line.end.x - line.begin.x);
+		double a = abs((thisSlope - otherSlope)/(1 + thisSlope * otherSlope));
 		return atan(a);
 
 	}
