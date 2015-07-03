ec2ea2a48de0566bf80399fb37a0c01b10a2a9f7
Rian van Gijlswijk <rian@vangijlswijk.com>
2015-07-03 20:22:44 +0300

    Added validation test for absorption from withers

--- test/ValidationTest.cpp
+++ test/ValidationTest.cpp
@@ -2,6 +2,7 @@
 #include <list>
 #include "../src/scene/Ionosphere.h"
 #include "../src/tracer/Ray.h"
+#include "../src/core/Application.h"
 #include "../src/core/Config.h"
 #include "../src/exporter/Data.h"
 #include "../src/exporter/MatlabExporter.h"
@@ -14,16 +15,18 @@ namespace {
 	using namespace raytracer::core;
 
 	class ValidationTest : public ::testing::Test {
+
+		public:
+			double R = 3390e3;
 	};
 
-	TEST_F(ValidationTest, ElectronDensityProfiles) {
+	TEST_F(ValidationTest, DISABLED_ElectronDensityProfiles) {
 
 		list<Data> dataSet;
 
 		Config celestialConfig = Config("config/scenario_LS180_SA75_DS0.json");
 
 		double SZA = 71 * Constants::PI / 180;
-		double R = 3390e3;
 
 		int dh = 100;
 		const Json::Value ionosphereConfig = celestialConfig.getObject("ionosphere")["layers"];
@@ -62,13 +65,11 @@ namespace {
 		me.dump("Debug/data_LS180_SA75_DS0_SZA71.dat", dataSet);
 	}
 
-	TEST_F(ValidationTest, ElectronDensityProfilesScanSZA) {
+	TEST_F(ValidationTest, DISABLED_ElectronDensityProfilesScanSZA) {
 
 		list<Data> dataSet;
 
 		Config celestialConfig = Config("config/scenario_LS180_SA75_DS0.json");
-
-		double R = 3390e3;
 
 		int dh = 100;
 		const Json::Value ionosphereConfig = celestialConfig.getObject("ionosphere")["layers"];
@@ -110,13 +111,12 @@ namespace {
 		me.dump("Debug/data_LS180_SA75_DS0_ScanSZA.dat", dataSet);
 	}
 
-	TEST_F(ValidationTest, Nielsen2007AbsorptionScanSZA) {
+	TEST_F(ValidationTest, DISABLED_Nielsen2007AbsorptionScanSZA) {
 
 		list<Data> dataSet;
 
 		Config celestialConfig = Config("config/scenario_nielsen2007.json");
 
-		double R = 3390e3;
 		double angularStepSize = Constants::PI/180;
 
 		int dh = 1000;
@@ -130,7 +130,6 @@ namespace {
 				r.rayNumber = ++numRay;
 				r.frequency = f;
 				r.o = Vector3d(0, 0, 0);
-				//r.d = Vector3d(sin(SZA), cos(SZA), 0).norm();
 				r.setAngle(Constants::PI/2 - SZA);
 				Vector3d N = Vector3d(sin(SZA), cos(SZA), 0).norm();
 
@@ -173,7 +172,74 @@ namespace {
 			}
 		}
 
-		MatlabExporter me;
+		MatlabExporter me("Debug/data_nielsen2007absorption.dat");
 		me.dump("Debug/data_nielsen2007absorption.dat", dataSet);
+	}
+
+	TEST_F(ValidationTest, Withers2011AbsorptionTest) {
+
+		list<Data> dataSet;
+		float SZA = 0;
+		int numRay = 0;
+		int dh = 100;
+		MatlabExporter me("Debug/data_withers2011_Meteoric.dat");
+
+		Config celestialConfig = Config("config/scenario_withers2011_M2.json");
+		Application::getInstance().setCelestialConfig(celestialConfig);
+		const Json::Value ionosphereConfig = celestialConfig.getObject("ionosphere")["layers"];
+
+		double peakProductionAltitude = 85e3;//ionosphereConfig[0].get("peakProductionAltitude", "").asDouble();
+		double neutralScaleHeight = ionosphereConfig[0].get("neutralScaleHeight", "").asDouble();
+
+		// assume single-layer ionosphere as that is how withers models the absorption
+		if (ionosphereConfig.size() > 1) {
+			std::cerr << "ionosphere config has too many layers! Maximum is 1 for this validation test.\n";
+		}
+
+		for (int m = 7; m <= 12; m++) {
+
+			double electronPeakDensity = pow(10, m);
+			double n = 6.0;
+
+			while (n < 11.0) {
+
+				double f = pow(10, n);
+				n += 0.01;
+
+				Ray r;
+				r.rayNumber = ++numRay;
+				r.frequency = f;
+				r.o = Vector3d(0, 0, 0);
+				r.setAngle(Constants::PI/2 - SZA);
+				Vector3d N = Vector3d(sin(SZA), cos(SZA), 0).norm();
+
+				for (int h = 70e3; h < 250e3; h += dh) {
+					Plane3d mesh = Plane3d(N, Vector3d((R+h)*N.x, (R+h)*N.y, (R+h)*N.z));
+					mesh.size = 1000;
+					Ionosphere io = Ionosphere(mesh);
+					io.layerHeight = dh;
+
+					ASSERT_NEAR(SZA, io.getMesh().normal.angle(Vector3d::SUBSOLAR), 0.001);
+
+					io.superimposeElectronNumberDensity(electronPeakDensity, peakProductionAltitude, neutralScaleHeight);
+
+					if (io.determineWaveBehaviour(&r) == Ray::wave_refraction) {
+						io.attenuate(&r);
+					} else {
+						r.signalPower = 0;
+					}
+				}
+
+				Data d;
+				d.rayNumber = r.rayNumber;
+				d.frequency = r.frequency;
+				d.signalPower = r.signalPower;
+				d.theta_0 = SZA;
+				d.n_e = electronPeakDensity;
+				dataSet.push_back(d);
+			}
+		}
+
+		me.dump("Debug/data_withers2011_Meteoric.dat", dataSet);
 	}
 }
